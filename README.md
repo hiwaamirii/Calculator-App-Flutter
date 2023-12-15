baa4156656274de9142924d849e2e1d354ac3fcf
hiwaamirii <hiwaamiri.ir@gmail.com>
2023-12-15 18:26:47 +0330

    README File Edit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-Simple Calculator App With Flutter
-Follow Me On Instagram: @hiwaamiri.ir
+# Calculator App With Flutter
+
+Simple calculator app with standard UI
+
+## Developer
+
+This project was developed by Hiwa Amiri
+* [Follow Me On Instagram](https://instagram.com/hiwaamiri.ir/) - @hiwaamiri.ir
+
+
+## Built With
+
+* [Flutter](https://flutter.dev/) - The framework used
+
+## License
+
+You can use this project for free
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -25,6 +24,87 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  final List<String> operators = ["+", "-", "x", "÷"];
+  final List<String> hist = [];
+  var history = "", output = "", answer = 0.0;
+
+  FocusNode clearButtonFocus = FocusNode();
+
+  void click(int number) {
+    setState(() {
+      if (double.parse(output) != 0.0) {
+        output += number.toString();
+      } else {
+        output = number.toString();
+      }
+    });
+  }
+
+  void clickDot() {
+    setState(() {
+      output += ".";
+    });
+  }
+
+  void clear() {
+    setState(() {
+      history = "";
+      output = "0";
+      answer = 0.0;
+      hist.clear();
+    });
+  }
+
+  void sign() {
+    setState(() {
+      if (double.parse(output) != 0.0) {
+        output = (output[0] == '-') ? output.substring(1) : "-$output";
+      }
+    });
+  }
+
+  void percent() {
+    setState(() {
+      final double percent = answer = answer / 100;
+      history = '$answer ÷ 100 =';
+      output = percent.toString();
+    });
+  }
+
+  String getTape() {
+    return hist.join(" ");
+  }
+
+  bool isOperator(String s) {
+    return operators.contains(s);
+  }
+
+  void equals() {
+    setState(() {
+      if (hist.length <= 3) {
+        hist.add(output);
+      }
+      history = '${getTape()} =';
+      final opr1 = double.parse(hist.removeAt(0));
+      final op = hist.removeAt(0);
+      final opr2 = double.parse(hist.removeAt(0));
+      switch (op) {
+        case "+":
+          answer = opr1 + opr2;
+          break;
+        case "-":
+          answer = opr1 - opr2;
+          break;
+        case "*":
+          answer = opr1 * opr2;
+          break;
+        case "÷":
+          answer = opr1 / opr2;
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
